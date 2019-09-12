0181b1f63ef5156612914b7f078034eeaf746b2d
V7 <chenv7v7@163.com>
2019-09-12 10:00:31 +0800

    初始化2.0

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,15 +1,68 @@
 <template>
-    <div>
-        酒店首页
-    </div>
+  <div class="hotel">
+    <!-- 面包屑区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right" class="mianbao">
+      <el-breadcrumb-item>酒店</el-breadcrumb-item>
+      <el-breadcrumb-item>南京市酒店预订</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 输入框内容区 -->
+    <el-row type="flex">
+      <el-col :span="4">
+        <el-input placeholder="目的地"></el-input>
+      </el-col>
+      <el-col :span="16">
+        <el-date-picker
+          v-model="value6"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+        <el-select v-model="value7" placeholder="请选择">
+          <el-option-group v-for="group in options3" :key="group.label" :label="group.label">
+            <el-option
+              v-for="item in group.options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-option-group>
+        </el-select>
+      </el-col>
+      <el-col :span="4"></el-col>
+
+      <!--  -->
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      value6: "",
+      options3: [{
+          label: '热门城市',
+          options: [{
+            value: 'Shanghai',
+            label: '上海'
+          }, {
+            value: 'Beijing',
+            label: '北京'
+          }]
+        }],
+        value7: ''
+    };
+  }
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.hotel {
+  width: 1000px;
+  margin: 0 auto;
+  .mianbao {
+    padding: 15px 0;
+  }
+}
 </style>
