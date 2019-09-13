97f549279105809530194366ca574035f7ebfb97
lily <943165920@qq.com>
2019-09-13 12:25:15 +0800

    优化发表攻略页的删除草稿箱功能

--- store/post.js
+++ store/post.js
@@ -14,8 +14,13 @@ export const mutations = {
     },
 
     //设置草稿箱标题
-    setDraftsTitle(state, data) {
-        state.draftsTitle = data
+    setDraftsTitle(state,data) {
+        state.draftsTitle.unshift(data)
+    },
+
+    //删除草稿箱
+    deleteDraftsTitle(state,index) {
+        state.draftsTitle.splice(index,1)
     }
 };
 export const actions = {
