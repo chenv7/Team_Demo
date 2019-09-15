d6b5f76771189a45164f396c1f90502bed8303dd
肖嘉豪 <1148505877@qq.con>
2019-09-15 17:01:04 +0800

    酒店详情地图点标志景点与路切换完成

--- pages/hotel/sldkfjase.vue
+++ pages/hotel/sldkfjase.vue
@@ -19,7 +19,8 @@
           <span class="colote el-icon-star-on"></span>
           <div class="textying">{{hotelData.alias}}</div>
           <div>
-            <span class="el-icon-location-outline"></span>{{hotelData.address}}
+            <span class="el-icon-location-outline"></span>
+            {{hotelData.address}}
           </div>
         </div>
       </el-row>
@@ -42,7 +43,7 @@
       </div>
     </div>
     <div class="various_price">
-      <el-table :data="tableData" style="width: 100%">
+      <el-table class="bianshou" @row-click="dianji" :data="tableData" style="width: 100%">
         <el-table-column prop="name" label="价格来源" width="400"></el-table-column>
         <el-table-column prop="bestType" label="低价房型" width="400"></el-table-column>
         <el-table-column label="低价房型" class="colorred">
@@ -55,14 +56,18 @@
     <div class="maplan">
       <el-row class="mapda">
         <el-col class="map_ditu" :span="16">
+          <div class="dituweiz"></div>
           <div id="container"></div>
         </el-col>
         <el-col class="map_right" :span="8">
           <el-tabs v-model="activeName">
             <el-tab-pane label="风景" name="first">
               <div class="onedemo">
-                <p v-for="(item, index) in landscape" :key="index">{{item.name}}</p>
-                
+                <p
+                  @mouseenter="zuobiao(index)"
+                  v-for="(item, index) in landscape"
+                  :key="index"
+                >{{item.name}}</p>
               </div>
             </el-tab-pane>
             <el-tab-pane label="交通" name="second">
@@ -82,56 +87,101 @@ export default {
   data() {
     return {
       tableData: [],
-      activeName: 'first',
+      activeName: "first",
       hotelData: {},
-      landscape: [],
-      jindainjiao: []
+      landscape: {},
+      jindainjiao: [],
+      buting: [],
+      shunbb: []
     };
   },
-  mounted() {
-    console.log(this.hotelData, '12')
-    let appe = this.hotelData
-    window.onLoad = function() {
-      console.log(this_.appe, '123')
-      var map = new AMap.Map("container");
-      //设置中心点坐标
-      var map = new AMap.Map("container", {
-        zoom: 11, //级别
-        center: [appe.location.latitude, appe.location.longitude], //中心点坐标
-        viewMode: "3D" //使用3D视图
-      });
-    };
-    var url =
-      "https://webapi.amap.com/maps?v=1.4.15&key=您申请的key值&callback=onLoad";
-    var jsapi = document.createElement("script");
-    jsapi.charset = "utf-8";
-    jsapi.src = url;
-    document.head.appendChild(jsapi);
 
+  mounted() {
     this.$axios({
-      url: '/hotels',
+      url: "/hotels",
       params: {
         id: 1
       }
-    }).then(res=>{
-      console.log(res)
-      this.hotelData = res.data.data[0]
-      console.log(this.hotelData.location)
-      this.tableData = res.data.data[0].products
-      console.log(this.hotelData.real_city)
-    })
+    }).then(res => {
+      console.log(res, "445");
+      this.hotelData = res.data.data[0];
+      this.tableData = res.data.data[0].products;
+      this.$store.commit("ditu/ditujuzhon", this.hotelData);
+    });
     this.$axios({
-      url: 'https://restapi.amap.com/v3/place/text?keywords=&city=%E5%8D%97%E4%BA%AC&location=118.871811,31.328468&types=%E9%A3%8E%E6%99%AF%E5%90%8D%E8%83%9C&output=json&page=1&offset=10&key=79041dfa1c752f49599e2b507c64da42',
-    }).then(res=>{
-      console.log(res)
-      this.landscape = res.data.pois
-    })
+      url:
+        "https://restapi.amap.com/v3/place/text?keywords=&city=%E5%8D%97%E4%BA%AC&location=118.871811,31.328468&types=%E9%A3%8E%E6%99%AF%E5%90%8D%E8%83%9C&output=json&page=1&offset=10&key=79041dfa1c752f49599e2b507c64da42"
+    }).then(res => {
+      this.landscape = res.data.pois;
+    });
     this.$axios({
-      url: 'https://restapi.amap.com/v3/place/text?keywords=&city=%E5%8D%97%E4%BA%AC&location=118.871811,31.328468&types=%E4%BA%A4%E9%80%9A%E8%AE%BE%E6%96%BD%E6%9C%8D%E5%8A%A1&output=json&page=1&offset=10&key=79041dfa1c752f49599e2b507c64da42'
-    }).then(res=>{
-      console.log(res)
-      this.jindainjiao = res.data.pois
-    })
+      url:
+        "https://restapi.amap.com/v3/place/text?keywords=&city=%E5%8D%97%E4%BA%AC&location=118.871811,31.328468&types=%E4%BA%A4%E9%80%9A%E8%AE%BE%E6%96%BD%E6%9C%8D%E5%8A%A1&output=json&page=1&offset=10&key=79041dfa1c752f49599e2b507c64da42"
+    }).then(res => {
+      console.log(res);
+      this.jindainjiao = res.data.pois;
+    });
+  },
+  updated() {
+    this.loadmap();
+    if (this.activeName === "first") {
+      this.buting = this.landscape;
+      return;
+    } else {
+      return (this.buting = this.jindainjiao);
+    }
+  },
+  methods: {
+    loadmap() {
+      let _this = this;
+      window.onLoad = function() {
+        console.log(_this.hotelData, "4444");
+        console.log(_this.biaosize, "chengg");
+        var map = new AMap.Map("container");
+        var map = new AMap.Map("container", {
+          zoom: 15, //级别
+          center: [118.874503, 31.329444], //中心点坐标
+          viewMode: "3D" //使用3D视图
+        });
+        console.log(_this.landscape, "进店");
+        let shunboj = [];
+        for (let i = 0; i < _this.buting.length; i++) {
+          shunboj.push(_this.buting[i].location.split(",").map(Number));
+        }
+        console.log(shunboj, "shun");
+        let fuckyou = [];
+        for (let i = 0; i < shunboj.length; i++) {
+          var marker = new AMap.Marker({
+            position: new AMap.LngLat(...shunboj[i]), // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+            title: _this.buting[i].name
+          });
+          fuckyou.push(marker);
+        }
+        // 将创建的点标记添加到已有的地图实例：
+        map.add(fuckyou);
+        function zuobiao(index) {
+          console.log(index)
+        }
+      };
+      var url =
+        "https://webapi.amap.com/maps?v=1.4.15&key=	744f1f3c00c95ef7123cb8ede6aa2a32&callback=onLoad";
+      var jsapi = document.createElement("script");
+      jsapi.charset = "utf-8";
+      jsapi.src = url;
+      document.head.appendChild(jsapi);
+    },
+    zuobiao(index) {
+      // console.log(this.$store.state.ditu.ditusize[0])
+      this.biaosize = this.landscape[index].location;
+      this.shunbb = this.biaosize.split(",").map(Number);
+      console.log(this.shunbb, "数组");
+      console.log(this.biaosize);
+
+      // console.log(ditusiz,'33');
+    },
+    dianji() {
+      window.open("https://hotels.ctrip.com/hotel/694679.html");
+    }
   }
 };
 </script>
@@ -200,19 +250,33 @@ export default {
   }
   .various_price {
     margin: 30px 0;
+    .bianshou {
+      cursor: pointer;
+    }
   }
   .spanred {
     color: #f90;
   }
   .maplan {
     .mapda {
+      .map_ditu {
+        position: relative;
+        .dituweiz {
+          width: 30px;
+          height: 30px;
+          background-color: red;
+          position: absolute;
+          top: 424px;
+          left: 265px;
+        }
+      }
       .map_right {
         height: 360px;
         .onedemo {
           padding: 10px;
           height: 280px;
-          overflow: scroll; 
-          p{
+          overflow: scroll;
+          p {
             margin: 12px 0;
             font-size: 14px;
             color: #666;
@@ -221,8 +285,8 @@ export default {
         .tewdemo {
           padding: 10px;
           height: 280px;
-          overflow: scroll; 
-          p{
+          overflow: scroll;
+          p {
             margin: 12px 0;
             font-size: 14px;
             color: #666;
---
+++ store/ditu.js
@@ -0,0 +1,10 @@
+export const state = () => {
+    return {
+        ditusize: {}
+    }
+}
+export const mutations = {
+    ditujuzhon(state, data) {
+        state.ditusize = data
+    }
+}
